6c38d25cd2efedb851942d3130043f764b682a4d
tanguy Colou <tanguy.colou@skema.edu>
2018-11-19 12:56:58 +0100

    First commit

---
+++ lib/cluedo.rb
@@ -0,0 +1,8 @@
+# SUDO CODE
+
+# SET UP
+
+# 1 LU écrit le nom des joueurs restants dans le sens du jeu en commençant par lui
+# 2 LU écrit qui commence.
+# 3 LU dit quelles sont ses trois cartes pour les rayer automatiquement.
+
