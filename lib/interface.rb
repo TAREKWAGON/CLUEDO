00c96afb726e4dbca1728718f4d77f86e00df671
tanguy Colou <tanguy.colou@skema.edu>
2018-11-19 13:45:07 +0100

    added the method to sort the players

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,24 +1,33 @@
 puts "welcome to CLUEDO"
 
 puts "Choose your character"
-u_name = gets.chomp
-
-puts "Choose character of player 1"
-1_name = gets.chomp
+name_a = gets.chomp
 
 puts "Choose character of player 2"
-2_name = gets.chomp
+name_b = gets.chomp
 
 puts "Choose character of player 3"
-3_name = gets.chomp
+name_c = gets.chomp
 
-puts "Choose character of player 4"
-4_name = gets.chomp
-
-puts "Choose character of player 5"
-5_name = gets.chomp
+puts "---"
+puts array_player = [name_a, name_b, name_c]
+puts "---"
 
 puts "Chose who start"
 starter = gets.chomp
-  if starter
+
+sort_array_player = []
+array_player.map! do |name|
+  if name == starter && name == name_a
+    sort_array_player = [name_a, name_b, name_c]
+  elsif name == starter && name == name_b
+    sort_array_player = [name_b, name_c, name_a]
+  elsif name == starter && name == name_c
+    sort_array_player = [name_c, name_a, name_b]
+  end
+end
+
+puts sort_array_player
+
+
 
