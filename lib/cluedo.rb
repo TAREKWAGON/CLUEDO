ab54d8ef76fd1d967915779b7e8b540bb6790c2d
tanguy Colou <tanguy.colou@skema.edu>
2018-11-19 13:09:50 +0100

    done

--- lib/cluedo.rb
+++ lib/cluedo.rb
@@ -2,8 +2,8 @@
 
 # SET UP
 
-# 1 LU écrit le nom des joueurs restants dans le sens du jeu en commençant par lui
+# 1 LU écrit le nom des joueurs dans le sens du jeu en commençant par lui
 # 2 LU écrit qui commence.
 # 3 LU dit quelles sont ses trois cartes pour les rayer automatiquement.
 
-#hey
+
---
+++ lib/interface.rb
@@ -0,0 +1,24 @@
+puts "welcome to CLUEDO"
+
+puts "Choose your character"
+u_name = gets.chomp
+
+puts "Choose character of player 1"
+1_name = gets.chomp
+
+puts "Choose character of player 2"
+2_name = gets.chomp
+
+puts "Choose character of player 3"
+3_name = gets.chomp
+
+puts "Choose character of player 4"
+4_name = gets.chomp
+
+puts "Choose character of player 5"
+5_name = gets.chomp
+
+puts "Chose who start"
+starter = gets.chomp
+  if starter
+
